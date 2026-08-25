61a5a88ac3b05a2a6dbf10dbf766866880db86e6
mui-1729 <s1f102503015@iniad.org>
2026-08-25 22:56:45 +0900

    refactor: progression定数の公開口を整理

--- src/progression/index.ts
+++ src/progression/index.ts
@@ -1,8 +1,12 @@
 export {
   BASE_PLAYER_HP,
+  DEFAULT_INITIAL_SKILL_IDS,
+  DEFAULT_INITIAL_STAGE_IDS,
   EXP_CURVE_FACTOR,
   HP_PER_LEVEL,
   POWER_MULTIPLIER_PER_LEVEL,
+} from './constants'
+export {
   addExp,
   createInitialPlayerProgress,
   getLevelForExp,
