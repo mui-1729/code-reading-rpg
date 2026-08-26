ead0967ffacd21b97692584ab913645344e319b5
mui-1729 <s1f102503015@iniad.org>
2026-08-26 14:13:55 +0900

    feat: 学習看板interactionを型で表現する

--- src/field/types.ts
+++ src/field/types.ts
@@ -12,11 +12,21 @@ export type BattleEntrance = FieldPosition & {
   label: string
 }
 
-export type SignInteraction = FieldPosition & {
+export type MessageSignInteraction = FieldPosition & {
   id: string
   kind: 'sign'
   message: string
+  learningHintId?: never
 }
+
+export type LearningSignInteraction = FieldPosition & {
+  id: string
+  kind: 'sign'
+  learningHintId: string
+  message?: never
+}
+
+export type SignInteraction = MessageSignInteraction | LearningSignInteraction
 
 export type NpcInteraction = FieldPosition & {
   id: string
