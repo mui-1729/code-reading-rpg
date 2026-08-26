c6b44278a5e5c9f7d36ed081782a4baf3ba38f23
mui-1729 <s1f102503015@iniad.org>
2026-08-26 11:33:10 +0900

    style: Battle報酬CSSを読み込む

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { AppRouter } from './AppRouter'
 import './styles.css'
 import './layout-fixes.css'
 import './stage-select.css'
+import './reward.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
