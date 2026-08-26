0238f1336f650cdebc31f1b625fecc799985bd00
mui-1729 <s1f102503015@iniad.org>
2026-08-27 02:58:09 +0900

    refactor: remove persistent quest tracker

--- src/RootLayout.tsx
+++ src/RootLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from '@tanstack/react-router'
 import { CodeCodex } from './learning/CodeCodex'
-import { QuestTracker } from './quests/QuestTracker'
 import { QuestVictoryFeedback } from './quests/QuestVictoryFeedback'
 
 export function RootLayout() {
@@ -8,7 +7,6 @@ export function RootLayout() {
     <>
       <Outlet />
       <QuestVictoryFeedback />
-      <QuestTracker />
       <CodeCodex />
     </>
   )
