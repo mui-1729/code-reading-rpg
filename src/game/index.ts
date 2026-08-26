082e8bb63f1575ec00645fdeb6fb0931fec3c378
mui-1729 <s1f102503015@iniad.org>
2026-08-26 13:50:49 +0900

    refactor: Area progression helperを公開する

--- src/game/index.ts
+++ src/game/index.ts
@@ -5,6 +5,7 @@ export {
   JAVASCRIPT_AREA_ID,
   TYPESCRIPT_AREA_ID,
 } from './areas'
+export { getAreaForBattle, getBattlesForArea, getBossBattleForArea } from './areaProgression'
 export { battles } from './battles'
 export { generateBattle } from './generator'
 export { skillDefinitionById, skillDefinitions } from './skillDefinitions'
@@ -12,7 +13,7 @@ export { getSkillCardForBattle, getSkillCardsForBattle, skills } from './skills'
 export { createSeededRandom } from './random'
 export { hasInitialValidTarget, isBattleSolvable } from './solvability'
 export { getTargets } from './targeting'
-export type { AreaAvailability, AreaDefinition } from './areas'
+export type { AreaAvailability, AreaDefinition, AreaRoutePath, AreaRoutes } from './areas'
 export type { CodeVariant, SkillDefinition } from './skillDefinitions'
 export type { Seed, SeededRandom } from './random'
 export type { Battle, Enemy, SkillCard, TargetRule } from './types'
