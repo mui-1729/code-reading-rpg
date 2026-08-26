8fcaaac715ed0c68b4a93d3c73dfe4536dafcc9b
mui-1729 <s1f102503015@iniad.org>
2026-08-26 18:44:11 +0900

    chore: export v3 progress schema

--- src/progression/index.ts
+++ src/progression/index.ts
@@ -26,7 +26,11 @@ export {
 } from './storage'
 export { ProgressProvider } from './ProgressProvider'
 export { useProgress } from './useProgress'
-export type { StoredPlayerProgressV1, StoredPlayerProgressV2 } from './storage'
+export type {
+  StoredPlayerProgressV1,
+  StoredPlayerProgressV2,
+  StoredPlayerProgressV3,
+} from './storage'
 export type {
   BattleVictoryInput,
   BattleVictoryResult,
