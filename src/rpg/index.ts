ddb1a358ff1ff4da77d2228df85276878202fcc4
mui-1729 <s1f102503015@iniad.org>
2026-08-28 03:19:12 +0900

    feat: export shared equipment presentation

--- src/rpg/index.ts
+++ src/rpg/index.ts
@@ -12,11 +12,25 @@ export type {
   EquipmentLoadout,
   EquipmentSlot,
 } from './equipment'
+export {
+  formatEquipmentBonuses,
+  formatEquipmentDelta,
+  getEquipmentPresentation,
+} from './equipmentPresentation'
+export type {
+  EquipmentPresentation,
+  EquipmentStatDelta,
+} from './equipmentPresentation'
 export { getCombatStats, getIncomingDamage, getSkillDamage } from './combat'
 export type { CombatStats } from './combat'
 export { getPartyFollowUpDamage, partyMemberById, partyMembers } from './party'
 export type { PartyMemberDefinition } from './party'
-export { characterVisuals, getWeaponVisual, weaponVisuals } from './visualAssets'
+export {
+  characterVisuals,
+  equipmentVisuals,
+  getEquipmentVisual,
+  getWeaponVisual,
+} from './visualAssets'
 export { RpgProvider } from './RpgProvider'
 export { useRpg } from './useRpg'
 export {
