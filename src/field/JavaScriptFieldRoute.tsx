d1690db53ca5a4bbd2cf4617be7eaaeef2bd3624
mui-1729 <s1f102503015@iniad.org>
2026-08-28 01:25:53 +0900

    refactor: remove unused JavaScript field route component

--- src/field/JavaScriptFieldRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { useBgm } from '../audio/useBgm'
-import { JavaScriptFieldPage } from './JavaScriptFieldPage'
-
-export function JavaScriptFieldRoute() {
-  useBgm('field')
-  return <JavaScriptFieldPage />
-}
