24ef88a93b4dec5702de99595977ca8d03473b0a
mui-1729 <s1f102503015@iniad.org>
2026-08-28 01:26:04 +0900

    refactor: remove unused TypeScript field route component

--- src/field/TypeScriptFieldRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { useBgm } from '../audio/useBgm'
-import { TypeScriptFieldPage } from './TypeScriptFieldPage'
-
-export function TypeScriptFieldRoute() {
-  useBgm('field')
-  return <TypeScriptFieldPage />
-}
