c81c6e5f370cf1d78695fd631cc0fb006762d16b
mui-1729 <s1f102503015@iniad.org>
2026-09-01 07:53:38 +0900

    feat: load authored enemy pixel details

--- src/main.tsx
+++ src/main.tsx
@@ -26,6 +26,7 @@ import './atlas-terrain.css'
 import './boss-guard.css'
 import './pixel-art.css'
 import './battle-presentation.css'
+import './battle-enemies.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
